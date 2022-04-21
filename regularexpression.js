8f0bb345a245918917c950ab282df6a2bda658c7
sgujuluva <sanghee04@gmail.com>
2022-04-21 15:54:06 +0200

    regular expression solved

--- regularexpression.js
+++ regularexpression.js
@@ -1,18 +1,17 @@
 /* Complete the function in the editor below by returning a RegExp object, , that matches any string  that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u. */
 
 function regexVar(string){
-
-    //for ( let i = 0; i < string.length; i++){
-
     let vowels = "aeiou";
-        //if(vowels.includes(string[0]) === vowels.includes(string[string.length -1])){
-            if(string.startsWith(vowels) === string.endsWith(vowels)){
+    let startVowel = string.slice(0,1);
+    //console.log(startVowel);
+    let endVowel = string.slice(-1);
+    //console.log(endVowel);
+      
+            if(startVowel === endVowel){
             return true;
         }else{
             return false;
-        }
-   // }
-
+        }  
 }
 console.log(regexVar("appla"));
 console.log(regexVar("bcd"));
